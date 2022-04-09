245c142ed6d54cf610fb82088ef5b90757596cb0
Jose Arcidiacono <j2arcidiacono@outlook.com>
2022-04-09 11:29:31 -0300

    primera consulta

---
+++ app/pgClient.js
@@ -0,0 +1,8 @@
+const { Client } = require('pg')
+const connectionString = `${process.env.DATABASE_URL}`
+
+const client = new Client({
+  connectionString
+})
+
+module.exports = client
--- app/routes/commentsService.js
+++ app/routes/commentsService.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const client = require('../pgClient')
 
 router.get('/get_comments/:post', (req, res) => {
+  client.connect()
+  client.query('SELECT NOW()', (err, res) => {
+    if (err == null) {
+      console.log(res.rows[0])
+    }
+    console.log(err, res)
+    client.end()
+  })
   return res.status(200).json({
     status: 'OK'
   })
