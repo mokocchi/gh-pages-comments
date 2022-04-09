69fc29bc74d577678b09cb8bfde1a787743d10c0
Jose Arcidiacono <j2arcidiacono@outlook.com>
2022-04-09 17:51:09 -0300

    add a comment (template)

--- app/routes/commentsService.js
+++ app/routes/commentsService.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { client, persist } = require('../sequelizeClient')
-const Comment = client.models.Comment
+const { persist } = require('../sequelizeClient')
+const { sequelize } = require('../models')
+const Comment = sequelize.models.Comment
 
 router.get('/get_comments/:post', (req, res) => {
-
+  return res.status(200).json({
+    status: 'OK'
+  })
 })
 
 router.post('/get_comments/:post', async (req, res) => {
-  await persist(() => Comment.create({
+  persist(() => Comment.create({
     userHandle: 'mokocchi',
     message: 'hey there!',
     email: 'j@ou.com',
@@ -18,10 +21,10 @@ router.post('/get_comments/:post', async (req, res) => {
     return res.status(200).json({
       status: 'OK'
     })
-  }).catch(err =>
-    console.log(err),
-  res.json('error')
-  )
+  }).catch(err => {
+    console.log(err)
+    res.json('error')
+  })
 })
 
 module.exports = router
