5c2aa6c63f42aeeb9a5a305e6466607e61d23e6f
Jose Arcidiacono <j2arcidiacono@outlook.com>
2022-08-11 15:10:59 -0300

    add ssl

--- app/sequelizeClient.js
+++ app/sequelizeClient.js
@@ -1,7 +1,14 @@
 const { Sequelize } = require('sequelize')
 console.log(process.env.DATABASE_URL)
-const connectionString = `${process.env.DATABASE_URL}?sslmode=require`
-const client = new Sequelize(connectionString, {})
+const connectionString = `${process.env.DATABASE_URL}`
+const client = new Sequelize(connectionString, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
 
 const persist = async (callback = () => {}) => {
   await client.sync({ force: false })
